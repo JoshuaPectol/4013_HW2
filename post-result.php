e12c05c192afa7a9af9d1e3f68b24af4f3bdcfc0
JoshuaPectol <61020071+JoshuaPectol@users.noreply.github.com>
2023-09-06 14:05:56 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -1,1 +1,17 @@
-
+<?php
+$pageTitle = "Post Result";
+include "view-header.php";
+?>
+<h1>Post Result</h1>
+<?php
+if(isset($_POST['my-name'])){
+?>
+  <p>The value sent is:</p>
+<?php
+  echo $_POST['my-name'];
+}
+else{
+  <p>Nothing posted to page<p>
+}
+include "view-footer.php";
+?>
